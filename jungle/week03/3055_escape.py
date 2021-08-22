08b83f0071f22bdf34ae100d3c16a21879e601a3
username <greenrock46@gmail.com>
2021-08-22 20:26:28 +0900

    bfs&dfs

--- jungle/week03/3055_escape.py
+++ jungle/week03/3055_escape.py
@@ -6,7 +6,7 @@ graph = [list(input()) for _ in range(R)]
 # 좌표 위에 뭔가 있으면 True로 방문 처리
 visited = [[False] * C for _ in range(R)]
 
-sony = deque()  # 고슴도치
+sonic = deque()  # 고슴도치
 water = deque()  # 물
 count = 0  # 시간(초) 카운트
 
@@ -21,21 +21,19 @@ for i in range(R):
             water.append((i, j))
             visited[i][j] = True
         elif graph[i][j] == 'S':
-            sony.append((i, j))
+            sonic.append((i, j))
             visited[i][j] = True
         elif graph[i][j] == 'X':
             visited[i][j] = True
 
 # 고슴도치 이동가능할때마다 반복문
-while sony:
+while sonic:
     # 물 현황 구현
-    water_loop = len(water)
-    for i in range(water_loop):
+    for i in range(len(water)):
         w_x, w_y = water.popleft()
         for j in range(4):
             nx = w_x + dx[j]
             ny = w_y + dy[j]
-            # if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != 'D' and visited[nx][ny] != True:
             if 0 <= nx < R and 0 <= ny < C:
                 if graph[nx][ny] == '.':
                     water.append((nx, ny))
@@ -46,15 +44,14 @@ while sony:
     count += 1
 
     # 고슴도치 움직이는 로직 구현
-    sony_loop = len(sony)
-    for _ in range(sony_loop):
-        s_x, s_y = sony.popleft()
+    for _ in range(len(sonic)):
+        s_x, s_y = sonic.popleft()
         for i in range(4):
             nx = s_x + dx[i]
             ny = s_y + dy[i]
             if 0 <= nx < R and 0 <= ny < C:
                 if graph[nx][ny] =='.' and visited[nx][ny] == False:
-                    sony.append((nx, ny))
+                    sonic.append((nx, ny))
                     visited[nx][ny] = True
                 elif graph[nx][ny] == 'D':
                     print(count)
