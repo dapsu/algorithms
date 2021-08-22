e2f8c55bb68fb7cdc03ce9517a8a9fba271e11d1
username <greenrock46@gmail.com>
2021-08-22 19:48:35 +0900

    bfs&dfs

--- jungle/week03/find_parent_node.py
+++ jungle/week03/find_parent_node.py
@@ -3,16 +3,19 @@ sys.setrecursionlimit(10**8)
 
 N = int(input())
 
-nodes = [[] for _ in range(N+1)]
-p_node = [0 for _ in range(N+1)]
+nodes = [[] for _ in range(N+1)]  # 각 노드별 연결되어 있는 노드들 추가. 리스트의 인덱스가 노드 번호
+p_node = [0] * (N+1)  # 노드의 부모 노드 표시
 
 for _ in range(N-1):
     a, b = map(int, input().split())
+    # 중복 상관없이 각 노드가 어떤 노드와 연결되어 있는지 모두 추가
     nodes[a].append(b)
     nodes[b].append(a)
 
 def dfs(start):
+    # 시작 노드와 연결된 모든 원소들 for문으로 확인
     for i in nodes[start]:
+        # p_node가 0이라면 start가 i번 노드의 부모 노드
         if p_node[i] == 0:
             p_node[i] = start
             dfs(i)
--- test/test.py
+++ test/test.py
@@ -1,39 +1,51 @@
-import sys
 from collections import deque
-m,n,h = map(int,input().split()) # mn크기, h상자수
-graph = []
-queue = deque([])
- 
-for i in range(h):
-    tmp = []
-    for j in range(n):
-        tmp.append(list(map(int,sys.stdin.readline().split())))
-        for k in range(m):
-            if tmp[j][k]==1:
-                queue.append([i,j,k])
-    graph.append(tmp)
-    
-dx = [-1,1,0,0,0,0]
-dy = [0,0,1,-1,0,0]
-dz = [0,0,0,0,1,-1]
-while(queue):
-    x,y,z = queue.popleft()
-    
-    for i in range(6):
-        a = x+dx[i]
-        b = y+dy[i]
-        c = z+dz[i]
-        if 0<=a<h and 0<=b<n and 0<=c<m and graph[a][b][c]==0:
-            queue.append([a,b,c])
-            graph[a][b][c] = graph[x][y][z]+1
-            
-day = 0
-for i in graph:
-    for j in i:
-        for k in j:
-            if k==0:
-                print(-1)
-                exit(0)
-        day = max(day,max(j))
-print(day-1)
- 
+
+R, C = map(int, input().split())
+matrix = [list(input()) for _ in range(R)]
+visited = [[False]*C for _ in range(R)]
+water = deque()
+sony = deque()
+
+for i in range(R):
+    for j in range(C):
+        if matrix[i][j] == 'S':
+            sony.append((i, j))
+            visited[i][j] = True
+        elif matrix[i][j] == '*':
+            water.append((i, j))
+            visited[i][j] = True
+        elif matrix[i][j] == 'X':
+            visited[i][j] = True
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+count = 0
+
+while sony:
+    for _ in range(len(water)):
+        x, y = water.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C:
+                if matrix[nx][ny] == '.':
+                    matrix[nx][ny] = '*'
+                    visited[nx][ny] = True
+                    water.append((nx, ny))
+
+    count += 1
+
+    for _ in range(len(sony)):
+        x, y = sony.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C and visited[nx][ny] == False:
+                if matrix[nx][ny] == '.':
+                    sony.append((nx, ny))
+                    visited[nx][ny] = True
+                elif matrix[nx][ny] == 'D':
+                    print(count)
+                    exit()
+print('KAKTUS')
