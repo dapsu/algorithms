3df02be06e7adbfdb552153a483e90721178f84c
username <greenrock46@gmail.com>
2021-08-29 17:49:58 +0900

    dp & greedy

--- jungle/week04/1700_lamp_adaptor_scheduling.py
+++ jungle/week04/1700_lamp_adaptor_scheduling.py
@@ -1,7 +1,13 @@
 N, K = map(int, input().split())
 scheduling = list(map(int, input().split()))
 
-adaptor = []
-for i in range(N):
-    if len(adaptor) < K:
+adaptor = [0] * N
+count = 0
+
+for i in scheduling:
+    if i in adaptor:
+        pass
+    elif 0 in adaptor:
+        adaptor[adaptor.index(0)] = i
+    else:  # 멀티탭에 빈자리 없고 현재 꽃혀 있는 전기용품들과 다를 때
         pass
--- test/test2.py
+++ test/test2.py
@@ -1,69 +1,25 @@
-def count (A, n):
-    count = 0
-    for i in range(n):
-        if ((A & (1 << i)) != 0):
-            count += 1
-    return count
-
-def isIn (i, A):
-    if ((A & (1 << (i - 2))) != 0):
-        return True
+from sys import stdin
+N, K = stdin.readline().split()
+N = int(N)
+K = int(K)
+multap = [0] * N
+li = list(map(int, stdin.readline().split()))
+res = swap = num = max_I = 0
+for i in li:
+    if i in multap:
+        pass
+    elif 0 in multap:
+        multap[multap.index(0)] = i
     else:
-        return False
-
-def diff (A, j):
-    t = 1 << (j - 2)
-    return (A & (~t))
-
-def minimum (W, D, i, A):
-    minValue = INF
-    minJ = 1
-    n = len(W) - 1
-    for j in range(2, n + 1):
-        if (isIn(j, A)):
-            m = W[i][j] + D[j][diff(A, j)]
-            if (minValue > m):
-                minValue = m
-                minJ = j
-    return minValue, minJ
-
-def travel (W):
-    n = len(W) - 1
-    size = 2 ** (n - 1)
-    D = [[0] * size for _ in range(n + 1)]
-    P = [[0] * size for _ in range(n + 1)]
-    for i in range(2, n + 1):
-        D[i][0] = W[i][1]
-    for k in range(1, n - 1):
-        for A in range(1, size):
-            if (count(A, n) == k):
-                for i in range(2, n + 1):
-                    if (not isIn(i, A)):
-                        D[i][A], P[i][A] = minimum(W, D, i, A)
-    A = size - 1
-    D[1][A], P[1][A] = minimum(W, D, 1, A)
-    return D, P
-
-INF = 1000000
-W = [
-    [-1, -1, -1, -1, -1],
-    [-1, 0, 2, 9, INF],
-    [-1, 1, 0, 6, 4],
-    [-1, INF, 7, 0, 8],
-    [-1, 6, 3, INF, 0]
-]
-
-D, P = travel(W)
-print('D =')
-for i in range(1, len(D)):
-    print(D[i])
-print('P =')
-for i in range(1, len(P)):
-    print(P[i])
-print('minlength =', D[1][2**(len(W)-2)-1])
-
-# N = int(input())  # 도시의 수
-# W = [list(map(int, input().split())) for _ in range(N)]
-# D, P = travel(W)
-
-# print('minlength =', D[1][2**(len(W)-2)-1])
+        for j in multap:
+            if j not in li[num:]:
+                swap = j
+                break
+            elif li[num:].index(j) > max_I:
+                max_I = li[num:].index(j)
+                swap = j
+        multap[multap.index(swap)] = i
+        max_I = swap = 0
+        res += 1
+    num += 1
+print(res)
