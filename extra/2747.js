de374f38983e6e9403affab0f4691e5a20dd4acd
username <greenrock46@gmail.com>
2022-03-09 20:57:54 +0900

    220309 js 문제 풀이

---
+++ extra/2747.js
@@ -0,0 +1,31 @@
+// https://www.acmicpc.net/problem/2747
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.srtout
+});
+
+let input;
+
+rl.on("line", (line) => {
+    // input = line;
+    input = parseInt(line);
+}).on("close", () => {
+    console.log(fibonacci(input));
+    process.exit();
+});
+
+const fibonacci = (num) => {
+    let dp = new Array(45).fill(0);
+    dp[1] = 1;
+    dp[2] = 1;
+
+    for (let i = 3; i <= num; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[num]; 
+}
+
+// console.log(fibonacci(10));
