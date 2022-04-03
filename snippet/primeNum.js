7fcb8e13bee266b1cacbfcd1fe692a6aee5f1b16
username <greenrock46@gmail.com>
2022-04-03 21:39:53 +0900

    스니펫 코드 수정

--- snippet/primeNum.js
+++ snippet/primeNum.js
@@ -1,9 +1,7 @@
 // 소수일 때 true 반환하는 함수
 function primeNum(num) {
     for (let i=2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
+        if (num % i === 0) return false;
     }
     return true;
 }
