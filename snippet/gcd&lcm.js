8e7110defdbd5b911160ec510cfa2cb1172b2059
username <greenrock46@gmail.com>
2022-04-22 11:42:51 +0900

    gcd&lcm 스니펫

--- snippet/gcd&lcm.js
+++ snippet/gcd&lcm.js
@@ -1,6 +1,9 @@
 const makeGcd = (m, n) => {
     const tmp = m % n;
     if (tmp === 0) return n;
-
     return makeGcd(n, tmp);
 };
+
+const makeLcm = (m, n) => {
+    return (m*n) / makeGcd(m, n);
+}
