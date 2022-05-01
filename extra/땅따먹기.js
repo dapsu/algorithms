db0e5c42fb4677bd5cb5bc4c891b5f14c18597cb
username <greenrock46@gmail.com>
2022-05-01 20:17:06 +0900

    220501 js알고리즘(dp)

--- extra/땅따먹기.js
+++ extra/땅따먹기.js
@@ -1,31 +1,44 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12913
 
-function solution(land) {
-    for (let i = 1; i < land.length; i++) {
-        land[i][0] += Math.max(
-            land[i-1][1],
-            land[i-1][2],
-            land[i-1][3]
-        );
-        land[i][1] += Math.max(
-            land[i-1][0],
-            land[i-1][2],
-            land[i-1][3]
-        );
-        land[i][2] += Math.max(
-            land[i-1][0],
-            land[i-1][1],
-            land[i-1][3]
-        );
-        land[i][3] += Math.max(
-            land[i-1][0],
-            land[i-1][1],
-            land[i-1][2]
-        );
-    }
+// sol.1
+// function solution(land) {
+//     for (let i = 1; i < land.length; i++) {
+//         land[i][0] += Math.max(
+//             land[i-1][1],
+//             land[i-1][2],
+//             land[i-1][3]
+//         );
+//         land[i][1] += Math.max(
+//             land[i-1][0],
+//             land[i-1][2],
+//             land[i-1][3]
+//         );
+//         land[i][2] += Math.max(
+//             land[i-1][0],
+//             land[i-1][1],
+//             land[i-1][3]
+//         );
+//         land[i][3] += Math.max(
+//             land[i-1][0],
+//             land[i-1][1],
+//             land[i-1][2]
+//         );
+//     }
 
-    return Math.max(...land[land.length-1]);
-}
+//     return Math.max(...land[land.length-1]);
+// }
+
+// sol.2
+const solution = (land) => {
+    return Math.max(...land.reduce((prev, curr) => {
+        return [
+            curr[0] + Math.max(prev[1], prev[2], prev[3]),
+            curr[1] + Math.max(prev[0], prev[2], prev[3]),
+            curr[2] + Math.max(prev[0], prev[1], prev[3]),
+            curr[3] + Math.max(prev[0], prev[1], prev[2]),
+        ];
+    }));
+};
 
 
 // testCase
