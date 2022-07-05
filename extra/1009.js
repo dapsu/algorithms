79f816d36415d50485b6dbd6bea5f37837012506
username <greenrock46@gmail.com>
2022-07-05 23:02:07 +0900

    22.07.05 js, 구현

--- extra/1009.js
+++ extra/1009.js
@@ -1,21 +1,33 @@
 // https://www.acmicpc.net/problem/1009
-// try again
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../../input.txt";
 const input = fs.readFileSync(filePath).toString().replace(/\r/g, '').split('\n');
 
 const T = Number(input.shift());
-const data = input.map(e => {
-  const [a, b] = e.split(' ').map(Number);
-    
-  let pow = 1;
+const data = input
+  .map(e => e.split(' ')
+    .map(Number)
+  );
 
-  for (let i = 0; i < b; i++) {
-    pow = (pow * a) % 10;
+const result = [];
+
+for (let i = 0; i < T; i++) {
+  const nums = [];
+  let startNum = 1;
+  
+  while (true) {
+    let value = String(data[i][0]**startNum);
+    if (nums.includes(value[value.length-1])) break;
+    nums.push(value[value.length-1]);
+    startNum++;
   }
-    
-  return pow === 0 ? 10 : pow;
-});
 
-console.log(data.join('\n'));
+  if (data[i][1] % nums.length === 0) {
+    if (nums[nums.length-1] === '0') result.push('10');
+    else result.push(nums[nums.length-1]);
+  }
+  else result.push(nums[(data[i][1] % nums.length) - 1]);
+}
+
+console.log(result.join('\n'));
