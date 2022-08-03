270357f695a5b84e481129945e4aaa3aa73a3f79
username <greenrock46@gmail.com>
2022-08-03 14:31:30 +0900

    22.08.03 js, 구현, linked list

--- js/81303_p.js
+++ js/81303_p.js
@@ -1,57 +1,81 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/81303
 
-function solution(n, k, cmds) {
-  /**
-   * n: 처음 표의 행 개수
-   * k: 처음에 선택된 행의 위치
-   * cmds: 명령어들이 담긴 문자열 배열
-   */
-  const state = new Array(n).fill().map(_ => 0);  // 행의 삭제 상태 파악. 숫자로 삭제된 순서 파악
-  let statePivot = 1;   // 삭제 상태에 순서 부여하기 위한 기준
-  const lengthTable = n - 1;  // 표 길이
-  
-  let curColumn = k;
-  cmds.forEach(value => {
-    let [command, num] = value.split(' ');
-    num = Number(num);
-    
-    if (command === 'D') {
-      let tmp = state.slice(curColumn, num + 1);
-      tmp = tmp.filter(num => num !== 0).length;
-      curColumn += num;
-      curColumn += tmp;
+class Node {
+  constructor(index, prevNode) {
+    this.index = index;
+    this.prev = prevNode;
+    this.next;
+  }
+}
+
+function solution(n, k, cmd) {
+  let answer = Array(n).fill("O");
+  let root = new Node(0);
+  let currentNode = root;
+  let prevNode = root;
+
+  for (let i = 1; i < n; i++) {
+    const newNode = new Node(i, prevNode);
+    prevNode.next = newNode;
+    prevNode = newNode;
+
+    if (i === k) {
+      currentNode = newNode;
     }
-    else if (command === 'U') {
-      let tmp = state.slice(num, curColumn);
-      tmp = tmp.filter(num => num !== 0).length;
-      curColumn -= num;
-      curColumn -= tmp;
-    }
-    else if (command === 'C') {
-      state[curColumn] = statePivot;
-      statePivot++;
-      if (curColumn === lengthTable) {
-        curColumn--;
-      }
-      else {
-        curColumn++;
-      }
-    }
-    else if (command === 'Z') {
-      const index = state.indexOf(--statePivot);
-      state[index] = 0;
+  }
+
+  const history = [];
+  cmd.map((current) => {
+    const [command, count] = current.split(" ");
+    let i = 0;
+    switch (command) {
+      case "U":
+        while (i < count && currentNode.prev) {
+          currentNode = currentNode.prev;
+          i++;
+        }
+        break;
+      case "D":
+        while (i < count && currentNode.next) {
+          currentNode = currentNode.next;
+          i++;
+        }
+        break;
+      case "C":
+        history.push(currentNode);
+        const prev = currentNode.prev;
+        const next = currentNode.next;
+        if (prev && next) {
+          prev.next = next;
+          next.prev = prev;
+          currentNode = next;
+        } else if (prev) {
+          prev.next = null;
+          currentNode = prev;
+        } else if (next) {
+          next.prev = null;
+          currentNode = next;
+        }
+        break;
+      case "Z":
+        const node = history.pop();
+        const prevNode = node.prev;
+        const nextNode = node.next;
+        if (prevNode) {
+          prevNode.next = node;
+        }
+        if (nextNode) {
+          nextNode.prev = node;
+        }
+        break;
     }
   });
-  const result = state.map(value => {
-    if (value === 0) {
-      return value = 'O';
-    }
-    else {
-      return value = 'X';
-    }
-  })
-    .join('');
-  return result;
+
+  history.map((node) => {
+    answer[node.index] = "X";
+  });
+
+  return answer.join("");
 }
 
 
