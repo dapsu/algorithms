cf08a0cf56474bdf4a4423eb9e45b87a4540e174
dapsu <greenrock46@gmail.com>
2022-10-13 11:26:00 +0900

    js, 완전탐색

--- ~2022/js/metoring.js
+++ ~2022/js/metoring.js
@@ -10,38 +10,56 @@ const testResults = [
   [4, 3, 2, 1],
   [3, 1, 4, 2],
 ];
+const removeDup = (arr) => {
+  return [...new Set(arr.join("|").split("|"))]
+    .map((v) => v.split(","))
+    .map((v) => v.map((a) => +a));
+};
 
 // my sol
-// function solution(arr, testResults) {
-//   const [n, m] = arr;
-  
-// }
-
-// ref sol
 function solution(arr, testResults) {
-  let answer = 0;
   const [n, m] = arr;
-  const tmp = [];
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
-      let count = 0;
-      for (let k = 0; k < m; k++) {
-        let pi = pj = 0;
-        for (let s = 0; s< n; s++) {
-          if (testResults[k][s] === i) pi = s;
-          if (testResults[k][s] === j) pj = s;
-        }
-        if (pi < pj) count++;
+  const pivotArr = testResults.shift();
+  const answer = [];
+  for (let i = 0; i < n - 1; i++) {
+    let p1 = pivotArr[i];
+    for (let j = i+1; j < n; j++) {
+      let p2 = pivotArr[j];
+      let flag = true;
+      for (let result of testResults) {
+        if (result.indexOf(p1) > result.indexOf(p2)) flag = false;
       }
-      if (count === m) {
-        tmp.push([i, j]);
-        answer++;
-      }
+      if (flag) answer.push([p1, p2]);
     }
   }
-  console.log(tmp);
-  return answer;
+  return removeDup(answer).length;
 }
+
+// ref sol
+// function solution(arr, testResults) {
+//   let answer = 0;
+//   const [n, m] = arr;
+//   const tmp = [];
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= n; j++) {
+//       let count = 0;
+//       for (let k = 0; k < m; k++) {
+//         let pi = pj = 0;
+//         for (let s = 0; s< n; s++) {
+//           if (testResults[k][s] === i) pi = s;
+//           if (testResults[k][s] === j) pj = s;
+//         }
+//         if (pi < pj) count++;
+//       }
+//       if (count === m) {
+//         tmp.push([i, j]);
+//         answer++;
+//       }
+//     }
+//   }
+//   console.log(tmp);
+//   return answer;
+// }
 
 // testCase
 console.log(solution(arr, testResults));  // 3
