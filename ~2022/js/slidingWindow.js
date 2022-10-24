785300f79ebbfadc7bf66b7399e59f73c08e3e92
dapsu <greenrock46@gmail.com>
2022-10-24 16:08:31 +0900

    js, sliding window

--- ~2022/js/slidingWindow.js
+++ ~2022/js/slidingWindow.js
@@ -6,21 +6,38 @@
  */
 
 // my sol
-function solution() {
-  
+function solution(n, k, arr) {
+  let maxNum = Number.MIN_SAFE_INTEGER;
+  const sumArr = [];
+  for (let i = 0; i < n; i++) {
+    if (sumArr.length > k) {
+      console.log('!!!Logic Error!!!');
+      return 'Logic Error';
+    }
+    sumArr.push(arr[i]);
+    if (sumArr.length < k) continue;
+    const sum = sumArr.reduce((a, b) => a + b, 0);
+    console.log();
+    console.log('sumArr:', sumArr);
+    console.log('sum:', sum);
+    console.log();
+    maxNum = Math.max(maxNum, sum);
+    sumArr.shift();
+  }
+  return maxNum;
 }
 
 // ref sol
-function solution(n, k, arr) {
-  let answer, sum = 0;
-  for (let i = 0; i < k; i++) sum += arr[i];
-  answer = sum;
-  for (let i = k; i < n; i++) {
-    sum += (arr[i] - arr[i - k]);
-    answer = Math.max(answer, sum);
-  }
-  return answer;
-}
+// function solution(n, k, arr) {
+//   let answer, sum = 0;
+//   for (let i = 0; i < k; i++) sum += arr[i];
+//   answer = sum;
+//   for (let i = k; i < n; i++) {
+//     sum += (arr[i] - arr[i - k]);
+//     answer = Math.max(answer, sum);
+//   }
+//   return answer;
+// }
 
 // testCase
 console.log(solution(10, 3, [12, 15 ,11, 20, 25, 10, 20, 19, 13, 15]));  // 56
