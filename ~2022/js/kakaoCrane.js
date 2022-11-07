867fdac0825dfa15d4f8fea52a4395d8e735aa52
dapsu <greenrock46@gmail.com>
2022-11-07 21:45:22 +0900

    js, stack, 구현

--- ~2022/js/kakaoCrane.js
+++ ~2022/js/kakaoCrane.js
@@ -28,9 +28,25 @@ function solution(board, moves) {
 }
 
 // ref sol
-// function solution() {
-
-// }
+function solution(board, moves) {
+  let answer = 0;
+  const stack = [];
+  moves.forEach(pos => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][pos-1] !== 0) {
+        let tmp = board[i][pos-1];
+        board[i][pos-1] = 0;
+        if (tmp === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        }
+        else stack.push(tmp);
+        break;
+      }
+    }
+  });
+  return answer;
+}
 
 // testCase
 console.log(solution([[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]], [1, 5, 3, 5, 1, 2, 1, 4]))     // 4
